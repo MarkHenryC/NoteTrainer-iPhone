cb9bd930f5671633f49c3038553c8a5266271b0e
MarkHenryC <mark.carolan@gmail.com>
2012-04-21 08:06:53 +1000

    Initial Commit

---
+++ NoteTrainer iPhone/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NoteTrainer iPhone
+//
+//  Created by Mark Carolan on 21/04/12.
+//  Copyright (c) 2012 DDD Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NoteTrainer iPhone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NoteTrainer iPhone
+//
+//  Created by Mark Carolan on 21/04/12.
+//  Copyright (c) 2012 DDD Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
